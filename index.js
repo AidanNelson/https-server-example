1839327a2e827395fefceaa2e39e10a5b35470c4
AidanNelson <AidanNelson@users.noreply.github.com>
2022-10-26 15:39:54 -0400

    add http server

--- index.js
+++ index.js
@@ -17,3 +17,8 @@ const PORT = process.env.PORT || 443;
 https
   .createServer(options, app)
   .listen(PORT, console.log(`server runs on port ${PORT}`));
+
+// Example with HTTP server running on 8080
+
+const http = require("http");
+http.createServer(app).listen(8080);
